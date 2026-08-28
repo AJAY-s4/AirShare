fb1069fdd8c9168322c8136c158e2989c035c8a6
AJAY <astro90986@gmail.com>
2026-08-28 23:03:50 +0530

    Fix Vercel build failure

--- airshare_frontend/build.sh
+++ airshare_frontend/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Download Flutter
-echo "Downloading Flutter SDK..."
-git clone https://github.com/flutter/flutter.git -b stable
+if [ ! -d "flutter" ]; then
+  echo "Downloading Flutter SDK..."
+  git clone https://github.com/flutter/flutter.git -b stable
+else
+  echo "Flutter SDK already exists. Skipping download..."
+fi
 
 # Add Flutter to Path
 export PATH="$PATH:`pwd`/flutter/bin"
